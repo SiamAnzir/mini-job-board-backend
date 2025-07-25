29393de97794e7f01309088ea824d8f6b245504e
SiamAnzir <siamanzir18@gmail.com>
2025-07-25 20:43:45 +0600

    added-auth-controller-file

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({ message: "User registered Successfully" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user || !(await user.comparePassword(req.body.password))) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
