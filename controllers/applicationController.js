75264668f633399019222e20f7cebad6b284855b
SiamAnzir <siamanzir18@gmail.com>
2025-07-26 13:40:11 +0600

    remove-file-upload-related-code

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,59 +1,24 @@
 const Job = require("../models/jobModel");
-const cloudinary = require("../config/cloudinary");
-const fs = require("fs");
 
 exports.submitApplication = async (req, res) => {
   try {
-    const { jobId, name, email, cvUrl } = req.body;
-    const file = req.file;
-    let cvLink = "";
+    const { jobId, name, email, cv } = req.body;
 
-    if (!jobId || !name || !email || !(file || cvUrl)) {
+    if (!jobId || !name || !email || !cv) {
       return res.status(400).json({
         message:
-          "Missing required fields: name, email, jobId and CV Url or file are required.",
-      });
-    }
-
-    if (file) {
-      const result = await new Promise((resolve, reject) => {
-        const uploadStream = cloudinary.uploader.upload_stream(
-          {
-            folder: "job-cvs",
-            resource_type: "raw",
-          },
-          (error, result) => {
-            if (error) return reject(error);
-            resolve(result);
-          }
-        );
-
-        fs.createReadStream(file.path).pipe(uploadStream);
-      });
-
-      cvLink = result.secure_url;
-    } else if (cvUrl) {
-      cvLink = cvUrl;
-    } else {
-      return res.status(400).json({
-        error: "Bad Request",
-        message: "Please provide a CV file or a valid CV URL.",
+          "Missing required fields: name, email, Job Id and CV Url are required.",
       });
     }
 
     const job = await Job.findById(jobId);
     if (!job) return res.status(404).json({ message: "Job not found" });
 
-    job.applicants.push({ name, email, cv: cvLink });
+    job.applicants.push({ name, email, cv });
     await job.save();
-
-    if (file) {
-      fs.unlinkSync(file.path);
-    }
 
     res.status(200).json({ message: "Application submitted successfully!" });
   } catch (err) {
-    console.log(err);
     res.status(500).json({ error: "Server Error", message: err.message });
   }
 };
--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,17 +1,8 @@
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
 const { submitApplication } = require("../controllers/applicationController");
 
 const router = express.Router();
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-const upload = multer({ storage });
 
-router.post("/", upload.single("cv"), submitApplication);
+router.post("/", submitApplication);
 
 module.exports = router;
